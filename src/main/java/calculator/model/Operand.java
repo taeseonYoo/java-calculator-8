ea3a8b0eaba515ade65eec6a0df74a9806a215be
taeseonYoo <jkl0642@hansung.ac.kr>
2025-10-16 00:48:01 +0900

    feat(Operand): 피연산자가 음수 또는 0 이면 예외 발생

--- src/main/java/calculator/model/Operand.java
+++ src/main/java/calculator/model/Operand.java
@@ -1,10 +1,19 @@
 package calculator.model;
 
 public class Operand {
-    private final int operand;
+    private int operand;
+
     public Operand(int operand) {
+        setOperand(operand);
+    }
+
+    private void setOperand(int operand) {
+        if (operand <= 0) {
+            throw new IllegalArgumentException("피연산자는 양수만 입력 가능합니다.");
+        }
         this.operand = operand;
     }
+
     public int getOperand() {
         return this.operand;
     }
