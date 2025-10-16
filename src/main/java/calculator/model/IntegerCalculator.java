a148bb298328ced837a0790379d865edd36e38ce
taeseonYoo <jkl0642@hansung.ac.kr>
2025-10-16 20:34:00 +0900

    refactor: 양수 검증의 책임을 Operand->IntegerCalculator로 변경

--- src/main/java/calculator/model/IntegerCalculator.java
+++ src/main/java/calculator/model/IntegerCalculator.java
@@ -6,9 +6,26 @@ public class IntegerCalculator implements Calculator {
     private final Operand[] operands;
 
     public IntegerCalculator(String inputValue) {
-        //TODO Separator로 operands를 초기화한다.
-        Separator separator = new Separator(inputValue);
-        operands = separator.split(inputValue);
+        if (verifyInputValueIsEmpty(inputValue)) {
+            operands = new Operand[1];
+            operands[0] = new Operand(0);
+        } else {
+            Separator separator = new Separator(inputValue);
+            operands = separator.split(inputValue);
+            verifyOperandIsPositive();
+        }
+    }
+
+    private boolean verifyInputValueIsEmpty(String inputValue) {
+        return inputValue.isEmpty();
+    }
+
+    private void verifyOperandIsPositive() {
+        for (Operand operand : operands) {
+            if (operand.getOperand() <= 0) {
+                throw new IllegalArgumentException("피연산자는 양수만 입력 가능합니다.");
+            }
+        }
     }
 
     @Override
--- src/main/java/calculator/model/Operand.java
+++ src/main/java/calculator/model/Operand.java
@@ -8,9 +8,6 @@ public class Operand {
     }
 
     private void setOperand(int operand) {
-        if (operand <= 0) {
-            throw new IllegalArgumentException("피연산자는 양수만 입력 가능합니다.");
-        }
         this.operand = operand;
     }
 
